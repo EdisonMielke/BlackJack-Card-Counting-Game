c8bf524da0eae09fd57f41d576fb8d538a884c09
Connduit <49081590+Connduit@users.noreply.github.com>
2021-05-30 12:09:12 -0700

    updated bs with dict instead of list

--- basic_strategy.py
+++ basic_strategy.py
@@ -1,106 +1,60 @@
 from hand import Hand
 from card import Card
-hard_total_matrix =  [
-                      #2           3            4              5              6              7            8          9          10         A
-                      #<8
-                      [["HIT"],    ["HIT"],     ["HIT"],       ["HIT"],       ["HIT"],       ["HIT"],    ["HIT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #9
-                      [["HIT"],    ["DOUBLE"],  ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"],    ["HIT"],    ["HOT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #10
-                      [["DOUBLE"], ["DOUBLE"],  ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"], ["DOUBLE"], ["DOUBLE"],["HIT"],   ["HIT"]],
-                      #11
-                      [["DOUBLE"], ["DOUBLE"],  ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"], ["DOUBLE"], ["DOUBLE"],["DOUBLE"],["HIT"]],
-                      #12
-                      [["HIT"],    ["HIT"],     ["STAND"],     ["STAND"],     ["STAND"],     ["HIT"],    ["HIT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #13
-                      [["STAND"],  ["STAND"],   ["STAND"],     ["STAND"],     ["STAND"],     ["HIT"],    ["HIT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #14
-                      [["STAND"],  ["STAND"],   ["STAND"],     ["STAND"],     ["STAND"],     ["HIT"],    ["HIT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #15
-                      [["STAND"],  ["STAND"],   ["STAND"],     ["STAND"],     ["STAND"],     ["HIT"],    ["HIT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #16
-                      [["STAND"],  ["STAND"],   ["STAND"],     ["STAND"],     ["STAND"],     ["HIT"],    ["HIT"],    ["HIT"],   ["HIT"],   ["HIT"]],
-                      #>17
-                      [["STAND"],  ["STAND"],   ["STAND"],     ["STAND"],     ["STAND"],     ["STAND"],  ["STAND"],  ["STAND"], ["STAND"], ["STAND"]],
-                     ]
 
-soft_total_matrix =  [
-                      #2             3              4              5              6           7          8          9          10         A
-                      #2
-                      [["HIT"],      ["HIT"],       ["HIT"],       ["DOUBLE"],    ["DOUBLE"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      #3
-                      [["HIT"],      ["HIT"],       ["HIT"],       ["DOUBLE"],    ["DOUBLE"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      #4
-                      [["HIT"],      ["HIT"],       ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      #5
-                      [["HIT"],      ["HIT"],       ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      #6
-                      [["HIT"],      ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      #7
-                      [["STAND"],    ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"],    ["DOUBLE"], ["STAND"], ["STAND"], ["HIT"],   ["HIT"],   ["HIT"]],
-                      #<8
-                      [["STAND"],    ["STAND"],     ["STAND"],     ["STAND"],     ["STAND"],  ["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"]],
-                     ]
+HIT = "H"
+STAND = "S"
+SPLIT = "P"
+DOUBLE = "D"
 
-double_matrix     =  [
-                      #2          3          4          5          6          7          8          9          10         A
-                      # 2,2
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      # 3,3
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      # 4,4
-                      [["HIT"],   ["HIT"],   ["HIT"],   ["SPLIT"], ["SPLIT"], ["SPLIT"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      # 5,5
-                      [["DOUBLE"],["DOUBLE"],["DOUBLE"],["DOUBLE"],["DOUBLE"],["DOUBLE"],["DOUBLE"],["DOUBLE"],["HIT"],   ["HIT"]],
-                      # 6,6
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      # 7,7
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["HIT"],   ["HIT"],   ["HIT"],   ["HIT"]],
-                      # 8,8
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"]],
-                      # 9,9
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["STAND"], ["SPLIT"], ["SPLIT"], ["STAND"], ["STAND"]],
-                      # 10,10
-                      [["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"], ["STAND"]],
-                      # A,A
-                      [["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"], ["SPLIT"]],
-                     ]
+HARD = {
+    21: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    20: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    19: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    18: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    17: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    16: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    15: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    14: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    13: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    12: {"2": HIT, "3": HIT, "4": STAND, "5": STAND, "6": STAND, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    11: {"2": DOUBLE, "3": DOUBLE, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": DOUBLE, "8": DOUBLE, "9": DOUBLE, "10": DOUBLE, "Jack": DOUBLE, "Queen": DOUBLE, "King": DOUBLE, "Ace": HIT},
+    10: {"2": DOUBLE, "3": DOUBLE, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": DOUBLE, "8": DOUBLE, "9": DOUBLE, "10": DOUBLE, "Jack": DOUBLE, "Queen": DOUBLE, "King": HIT, "Ace": HIT},
+    9: {"2": HIT, "3": DOUBLE, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    8: {"2": HIT, "3": HIT, "4": HIT, "5": HIT, "6": HIT, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    7: {"2": HIT, "3": HIT, "4": HIT, "5": HIT, "6": HIT, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    6: {"2": HIT, "3": HIT, "4": HIT, "5": HIT, "6": HIT, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    5: {"2": HIT, "3": HIT, "4": HIT, "5": HIT, "6": HIT, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+}
 
-def basic_strategy(hand, dealer_card) -> str:
-    ret_str = "ERROR"
+SOFT = {
+    21: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    20: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    19: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    18: {"2": STAND, "3": DOUBLE, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": STAND, "8": STAND, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    17: {"2": HIT, "3": DOUBLE, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    16: {"2": HIT, "3": HIT, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    15: {"2": HIT, "3": HIT, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    14: {"2": HIT, "3": HIT, "4": HIT, "5": DOUBLE, "6": DOUBLE, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    13: {"2": HIT, "3": HIT, "4": HIT, "5": DOUBLE, "6": DOUBLE, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+}
 
-    #Normal hand
-    if hand.value >= 17:
-        ret_str = hard_total_matrix[-1][dealer_card.value-2]
-    elif hand.value <= 8:
-        ret_str = hard_total_matrix[0][dealer_card.value - 2]
+PAIR = {
+    12: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": SPLIT, "8": SPLIT, "9": SPLIT, "10": SPLIT, "Jack": SPLIT, "Queen": SPLIT, "King": SPLIT, "Ace": SPLIT},
+    20: {"2": STAND, "3": STAND, "4": STAND, "5": STAND, "6": STAND, "7": STAND, "8": STAND, "9": STAND, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    18: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": STAND, "8": SPLIT, "9": SPLIT, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    16: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": SPLIT, "8": SPLIT, "9": SPLIT, "10": SPLIT, "Jack": SPLIT, "Queen": SPLIT, "King": SPLIT, "Ace": SPLIT},
+    14: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": SPLIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    12: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    10: {"2": DOUBLE, "3": DOUBLE, "4": DOUBLE, "5": DOUBLE, "6": DOUBLE, "7": DOUBLE, "8": DOUBLE, "9": DOUBLE, "10": STAND, "Jack": STAND, "Queen": STAND, "King": STAND, "Ace": STAND},
+    8: {"2": HIT, "3": HIT, "4": HIT, "5": SPLIT, "6": SPLIT, "7": HIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    6: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": SPLIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+    4: {"2": SPLIT, "3": SPLIT, "4": SPLIT, "5": SPLIT, "6": SPLIT, "7": SPLIT, "8": HIT, "9": HIT, "10": HIT, "Jack": HIT, "Queen": HIT, "King": HIT, "Ace": HIT},
+}
+
+def basic_strategy(hand: Hand, upcard: Card) -> str:
+    if hand.splittable():
+        return PAIR[hand.getValue()][upcard.rank]
+    elif hand.softAces:
+        return SOFT[hand.getValue()][upcard.rank]
     else:
-        ret_str = hard_total_matrix[hand.getValue() - 8][dealer_card.value-2]
-
-    #Ace hand
-    aceFlag = False
-    otherCardValue = 0
-    for i in range(len(hand.cards)):
-        if hand.cards[i].rank == "Ace":
-            aceFlag = True
-        else:
-            otherCardValue += hand.cards[i].value
-    if otherCardValue >= 8:
-        otherCardValue = -1
-    if aceFlag == True:
-        ret_str = soft_total_matrix[otherCardValue-2][dealer_card.value-2]
-
-    #Double Cards
-    if len(hand.cards) == 2:
-        if hand.cards[0].value == hand.cards[1].value:
-            ret_str = double_matrix[hand.cards[0].value - 2][dealer_card.value-2]
-    return ret_str
-
-#testing
-#if __name__ == '__main__':
-#    card1 = Card("Ace", "Spades")
-#    card2 = Card("7", "Hearts")
-#    cards = [card1, card2]
-#    hand = Hand(cards)
-#    dealer_card = Card("4", "Clubs")
-#    print(basic_strategy(hand, dealer_card))
+        return HARD[hand.getValue()][upcard.rank]
